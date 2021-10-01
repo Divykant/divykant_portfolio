c0fb30781048120b47eb98557b3ee98455f7dda9
Divykant <divykant1921107@akgec.ac.in>
2021-10-01 22:49:07 +0530

    Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigation } from './components/Navigation';
 function App() {
   return (
     <>
-    
-    <h2>hiiiiii</h2>
+    <Navigation />
     </>
   );
 }
